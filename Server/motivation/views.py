956b9ef14c0865c1cedc2598cdb69a99bfc3f29b
sivernorman <silvernorman.o@gmail.com>
2022-03-30 18:14:31 +0300

    updates

--- Server/motivation/views.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import context
-from django.shortcuts import render
-from motivation.models import Staff
-# Create your views here.
-
-def Staff(request,name):
-    staff = Staff.objects.get(name=name)
-    context = {'staff':staff}
-    return render(request,'staff.html')
