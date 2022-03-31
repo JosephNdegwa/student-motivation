a773a5b4801e7893df8a8b0296c0df43b59604a8
sivernorman <silvernorman.o@gmail.com>
2022-03-31 21:44:24 +0300

    added signin page

---
+++ Client/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  [x: string]: any;
+
+  constructor(private formBuilder:FormBuilder) { 
+  }
+
+  ngOnInit(): void {
+ ;
+   
+  }
+
+}
