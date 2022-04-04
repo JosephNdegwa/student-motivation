f09f8ad6ea558c58882c1ee3bbe35e8c5df45d5a
josephndegwa <kariukindegwa98@gmail.com>
2022-04-04 10:45:19 +0300

    update components on angular and change urls on server

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { MotivationComponent } from './motivation/motivation.component';
 import { RegisterComponent } from './register/register.component';
+
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'register', component:RegisterComponent},
+  {path: 'landing', component:LandingComponent},
+  {path: 'motivation', component:MotivationComponent}
 
 ];
 
