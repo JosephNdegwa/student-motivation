b221a7ba0057ca5fb13ef85a1941bdc26ca671e2
sivernorman <silvernorman.o@gmail.com>
2022-04-05 09:18:01 +0300

    added admin-dashboard

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { MotivationComponent } from './motivation/motivation.component';
 import { RegisterComponent } from './register/register.component';
 import { SingleMotivationComponent } from './single-motivation/single-motivation.component';
 
@@ -10,13 +12,10 @@ const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'register', component:RegisterComponent},
-<<<<<<< HEAD
-=======
   {path: 'landing', component:LandingComponent},
   {path: 'motivation', component:MotivationComponent},
   {path: 'single-motivation', component:SingleMotivationComponent},
   {path: 'admin-dashboard', component:AdminDashboardComponent}
->>>>>>> b38a996604f13451e641b087784f3ca0c536d2d8
 
 ];
 
