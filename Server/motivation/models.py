dbff06dde571c42e6db90fbf59311dca4560622c
Josephndegwa <Kariukindegwa98@gmail.com>
2022-04-12 18:01:50 +0300

    added server site

---
+++ Server/motivation/models.py
@@ -0,0 +1,105 @@
+from distutils.command.upload import upload
+from unicodedata import category
+from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from .forms import CustomUserManager
+# Create your models here.
+class StudentUser(AbstractBaseUser, PermissionsMixin):
+    ADMIN = 1
+    STUDENT = 2
+
+    USER_ROLE_CHOICES = (
+        (ADMIN, 'Staff'),
+        (STUDENT, 'Student'),
+    )
+    username = models.CharField(unique=True,max_length=20)
+    first_name = models.CharField(max_length=30, blank=True,null=True)
+    last_name = models.CharField(max_length=50, blank=True,null=True)
+    email = models.EmailField()
+    role = models.PositiveSmallIntegerField(choices=USER_ROLE_CHOICES, blank=True, null=True, default=2)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    pub_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+            return self.category_name
+
+# profile class
+class Profile(models.Model): 
+    user = models.OneToOneField(StudentUser, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image',blank=True,null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True,blank=True)
+    pub_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+       return self.user.username 
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=260)
+    article = HTMLField(blank=True)
+    image = CloudinaryField('images', blank=True,null=True)
+    video = models.FileField(upload_to='media/',blank=True,null=True)
+    audio_track = models.FileField(upload_to='media/', blank=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    pub_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True,blank=True)
+    updated = models.DateTimeField(auto_now=True)
+      
+    def __str__(self):
+       return self.title
+
+
+
+
+class Review(models.Model):
+    review = models.TextField()
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,null=True)
+    pub_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return self.profile.user.username
+
+# Review subclass
+class ReviewThread(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    content = models.TextField()
+    review = models.ForeignKey(Review,on_delete=models.CASCADE)
+    pub_at = models.DateTimeField(auto_now_add=True)
+
+class Subscription(models.Model):
+    user =models.ForeignKey(StudentUser,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.name
+
+class WishList(models.Model):
+   post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
+   profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
---
+++ Server/motivation/urls.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.urls import path,include
+from . import views
+from rest_framework import routers
+from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views
+from .views import(
+    AuthUserRegistrationView,AuthUserLoginView,
+    AuthLogoutView,UserListView,
+)
+urlpatterns=[
+    path('api/post/', views.post),
+    path('api/post/mot-id/<int:pk>', views.post_id, name='post_id'),
+    path('api/mot-cat/(?P<cat_pk>[0-9]+)', views.MotivationalByCategory.as_view()),
+    path('api/rev/', views.ReviewList.as_view()),
+    path('api/review/rev-id/(?P<pk>[0-9]+)/', views.ReviewDescription.as_view()),
+    path('api/category/', views.CategoryList.as_view()),
+    path('api/category/cat_idd/(?P<cat_pk>[0-9]+)', views.category_id),
+    path('api/review_thread/<int:id>',views.review_thread,name='review_thread'),
+    path('api/wishlist/<int:pk>',views.wishlist_motivation,name='wishlist'),
+    path('api/user_wishlist',views.all_wishlist,name='all user wishlist picks'),
+    path('api/profile/',views.profile, name='profile'),
+    path('api/token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register', AuthUserRegistrationView.as_view(), name='register'),
+    path('api/login', AuthUserLoginView.as_view(), name='login'),
+    path('api/logout',AuthLogoutView.as_view(),name='logout'),
+    path('api/subscribe/<int:pk>',views.subscription_service,name='category_subscription'),
+    path('api/users', views.all_users, name='users'),
+    path('api/remove_user/<int:id>',views.remove_user,name='user_deactivate'),
+    path('api/review/<int:id>', views.review,  name = 'review'),
+    path('api/current_user', views.current_user,name='current_user'),
+    path('api/superuser/<int:pk>',views.change_to_superuser,name='superuser_status'),
+
+]
