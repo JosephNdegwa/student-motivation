f955ef7c74c8b065547f34837ac8b39a31748d82
Josephndegwa <Kariukindegwa98@gmail.com>
2022-04-13 10:43:47 +0300

    added redirect file

---
+++ Server/motivation/email.py
@@ -0,0 +1,14 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+def send_welcome_email(name,receiver):
+    # subject and sender
+    subject = 'Welcome to the Stud Motive Family'
+    sender = 'add your email here'
+
+    #pass context vairables
+    text_content = render_to_string('email/studentemail.txt',{"name": name})
+    html_content = render_to_string('email/studentemail.html',{"name": name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
