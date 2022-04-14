1a3e92be930da5744323e0b664640b0ae27e98c7
Josephndegwa <Kariukindegwa98@gmail.com>
2022-04-14 11:29:09 +0300

    deployment

--- student/settings.py
+++ student/settings.py
@@ -17,6 +17,7 @@ import cloudinary.api
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
